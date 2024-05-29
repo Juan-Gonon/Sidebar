f947cce7413feb1830dfcd1e4066cb434388d97a
Juan Gonon <juanfegonon27@gmail.com>
2024-05-29 15:59:13 -0600

    sidebarButton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components'
 import logo from '../assets/react.svg'
 import { v } from '../styles/Variables'
+import { AiOutlineLeft } from 'react-icons/ai'
 
 // eslint-disable-next-line react/prop-types
 export const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   return (
     <Container $isOpen={sidebarOpen}>
-
+      <button className='sidebar__button'>
+        <AiOutlineLeft />
+      </button>
       <div className='logo__content'>
         <div className='img__content'>
           <img src={logo} alt='' />
@@ -23,6 +26,23 @@ const Container = styled.div`
   position: sticky;
   padding-top: 20px;
 
+  .sidebar__button{
+    position: absolute;
+    top: ${v.xxlSpacing};
+    right: -18px;
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.bgtgderecha};
+    box-shadow: 0 0 4px ${({ theme }) => theme.bg3}, 0 0 7px ${({ theme }) => theme.bg};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: transform .3s;
+    transform: ${({ $isOpen }) => ($isOpen ? 'initial' : 'rotate(180deg)')};
+  }
+
   .logo__content{
     display: flex;
     justify-content: center;
@@ -33,7 +53,7 @@ const Container = styled.div`
     .img__content{
       display: flex;
       cursor: pointer;
-      transition: all .3s;
+      transition: transform .3s;
       transform: ${({ $isOpen }) => ($isOpen ? 'scale(.7)' : 'scale(1.5)')};
 
       img{
